a6b9164951cef26b372c100c9cf9d331a9ee8dc8
franciscolopezsancho <franciscolopezsancho@gmail.com>
2020-04-16 11:46:54 -0500

    first commit. Just writing an idea what we want to achieve in the test

---
+++ src/test/scala/example/BoxSpec.scala
@@ -0,0 +1,10 @@
+package example
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class BoxSpec extends AnyFlatSpec with Matchers {
+  "The box object" should "accept Commands, transform them in events and reply" in {
+    
+  }
+}
