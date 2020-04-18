00dab1f9c4ed669e7c52b52577856c9341bace33
franciscolopezsancho <franciscolopezsancho@gmail.com>
2020-04-18 16:06:11 -0500

    cleaned 1

--- build.sbt
+++ build.sbt
@@ -11,9 +11,6 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-persistence-typed" % akkaVersion,
   "com.typesafe.akka" %% "akka-persistence-query" % akkaVersion,
   "ch.qos.logback" % "logback-classic" % "1.2.3",
-  "com.github.dnvriend" %% "akka-persistence-jdbc" % "3.5.2",
-  "mysql" % "mysql-connector-java" % "8.0.19",
-  "com.typesafe.akka" %% "akka-serialization-jackson" % "2.6.4",
   "com.typesafe.akka" %% "akka-actor-testkit-typed" % akkaVersion % Test,
   "org.scalatest" %% "scalatest" % "3.1.0" % Test
 )
--- src/main/scala/example/Box.scala
+++ src/main/scala/example/Box.scala
@@ -5,69 +5,60 @@ import akka.persistence.typed.scaladsl.Effect
 import akka.persistence.typed.PersistenceId
 import akka.actor.typed.Behavior
 import akka.actor.typed.ActorRef
-  
+
 object Box {
 
   //STATE
-  case class Item(description: String, size: Int)
-  final case class State(items: List[Item], maxCapacity: Int) {
+  case class Item(description: String)
+  final case class State(items: List[Item]) {
     def addItem(item: Item): State = {
       this.copy(items = item +: items)
     }
-    def roomLeft = maxCapacity - items.map(_.size).sum
   }
 
   object State {
-    def empty(maxCapacity: Int) = State(List.empty, maxCapacity)
+    def empty = State(List.empty)
   }
-
 
   //COMMANDS
   sealed trait Command
   case class AddItem(
       description: String,
-      size: Int,
-      replyTo: ActorRef[Confirmation]
+      replyTo: ActorRef[State]
   ) extends Command
 
   //EVENTS
   sealed trait Event {
     def boxId: String
   }
-  case class ItemAdded(boxId: String, description: String, size: Int) extends Event
+  case class ItemAdded(boxId: String, description: String) extends Event
 
-  //REPLIES
-  sealed trait Confirmation
-  case class Accepted(roomLeft: Int) extends Confirmation
-  case class Rejected(item: Item, roomLeft: Int) extends Confirmation
-
-  def apply(boxId: String, maxCapacity: Int): Behavior[Command] = {
+  def apply(boxId: String): Behavior[Command] = {
     EventSourcedBehavior[Command, Event, State](
       PersistenceId("Box", boxId),
-      State.empty(maxCapacity),
+      State.empty,
       (state, command) => commandHandler(boxId, state, command),
       (state, event) => eventHandler(state, event)
     )
   }
 
-  def commandHandler(boxId: String, state: State, command: Command): Effect[Event, State] = {
+  def commandHandler(
+      boxId: String,
+      state: State,
+      command: Command
+  ): Effect[Event, State] = {
     command match {
-      case AddItem(description, size, replyTo) => {
-        if (size < state.roomLeft) {
-          Effect
-            .persist(ItemAdded(boxId, description, size))
-            .thenRun(state => replyTo ! Accepted(state.roomLeft))
-        } else {
-          replyTo ! Rejected(Item(description, size), state.roomLeft)
-          Effect.none
-        }
+      case AddItem(description, replyTo) => {
+        Effect
+          .persist(ItemAdded(boxId, description))
+          .thenRun(state => replyTo ! state)
       }
     }
   }
   def eventHandler(state: State, event: Event): State = {
     event match {
-      case ItemAdded(boxId, description, size) => {
-        state.addItem(Item(description, size))
+      case ItemAdded(boxId, description) => {
+        state.addItem(Item(description))
       }
     }
   }
